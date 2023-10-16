c87aa79063eb3300f27f6c601c09cb57480061f1
cplmills <91394219+cplmills@users.noreply.github.com>
2023-10-16 14:53:53 +1000

    All lint errors fixed, prettier also run

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,21 +1,24 @@
 import { loadHeaderFooter, showCartCount } from "./utils.mjs";
-import { getLocalStorage , setLocalStorage} from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
-  if (localStorage.getItem("so-cart") === null  || cartItems.length === 0){  // if there are no items in the cart
+  if (localStorage.getItem("so-cart") === null || cartItems.length === 0) {
+    // if there are no items in the cart
     const emptyCartMessage = document.querySelector(".cart-heading");
     emptyCartMessage.innerHTML = "My Cart - You Have No Items In Your Cart";
     // document.getElementsByTagName("main")[0].appendChild(emptyCartMessage);
-  }else{
-    const htmlItems = cartItems.map((item, index) => cartItemTemplate(item, index));
+  } else {
+    const htmlItems = cartItems.map((item, index) =>
+      cartItemTemplate(item, index)
+    );
     document.querySelector(".product-list").innerHTML = htmlItems.join("");
     const removeButtons = document.querySelectorAll(".remove-button");
-  removeButtons.forEach((button, index) => {
-    button.addEventListener("click", () => {
-      removeCartItem(index);
+    removeButtons.forEach((button, index) => {
+      button.addEventListener("click", () => {
+        removeCartItem(index);
+      });
     });
-  });
     // Update the total price
     showTotalContents(cartItems);
   }
@@ -26,14 +29,16 @@ function cartItemTemplate(item, index) {
   let discountPercentage = 0;
   if (item.ListPrice > 300) {
     discountPercentage = 0.05;
-  } else if (item.ListPrice >150) {
+  } else if (item.ListPrice > 150) {
     discountPercentage = 0.03;
   }
   const discountPrice = item.ListPrice * discountPercentage;
-  const discountedPrice = item.ListPrice-discountPrice;
+  const discountedPrice = item.ListPrice - discountPrice;
   item.discountedPrice = discountedPrice;
   const newItem = `<li class="cart-card divider">
-  <button class="remove-button" data-index="${index}" data-id="${item.id}">&#10006;</button>
+  <button class="remove-button" data-index="${index}" data-id="${
+    item.id
+  }">&#10006;</button>
   <a href="#" class="cart-card__image">
     <img
       src="${item.Image}"
@@ -71,27 +76,29 @@ function removeCartItem(index) {
 
 function showTotalContents(items) {
   //ensure this only runs on the cart page
-  if (window.location.href.indexOf("cart.html")>0){
-    items.forEach((item)=> {
+  if (window.location.href.indexOf("cart.html") > 0) {
+    items.forEach((item) => {
       let discountPercentage = 0;
       if (item.ListPrice > 300) {
-      discountPercentage = 0.05;
-    } else if (item.ListPrice >150) {
-      discountPercentage = 0.03;
-    }
-    const discountPrice = item.ListPrice * discountPercentage;
-    const discountedPrice = item.ListPrice-discountPrice;
-    item.discountedPrice = discountedPrice;
-    })  
-    
-    if (items.length !== 0 ) {
+        discountPercentage = 0.05;
+      } else if (item.ListPrice > 150) {
+        discountPercentage = 0.03;
+      }
+      const discountPrice = item.ListPrice * discountPercentage;
+      const discountedPrice = item.ListPrice - discountPrice;
+      item.discountedPrice = discountedPrice;
+    });
+
+    if (items.length !== 0) {
       document.querySelector(".cart-footer.hide").style.display = "unset";
 
       const itemPricesList = items.map((item) => item.discountedPrice);
 
       const priceTotal = itemPricesList.reduce(
-        (item, currentTotal) => item + currentTotal, 0);
-      
+        (item, currentTotal) => item + currentTotal,
+        0
+      );
+
       // Clear the old total and insert the updated total
       const totalElement = document.querySelector(".cart-total");
       totalElement.innerHTML = `Total: $${priceTotal.toFixed(2)}`;
@@ -99,17 +106,15 @@ function showTotalContents(items) {
       // If there are no items, clear the total
       const totalElement = document.querySelector(".cart-total");
       totalElement.innerHTML = "";
-      
     }
   }
 }
 
 function checkCartItems() {
   const cartItems = getLocalStorage("so-cart");
-  if (cartItems != null){
+  if (cartItems != null) {
     showTotalContents(cartItems);
   }
-  
 }
 
 renderCartContents();
--- src/js/product.js
+++ src/js/product.js
@@ -1,25 +1,23 @@
-import { setLocalStorage } from "./utils.mjs";
-import { findProductById, getData } from "./productData.mjs";
-import { getLocalStorage } from "./utils.mjs";
+// import { setLocalStorage } from "./utils.mjs";
+// import { getLocalStorage } from "./utils.mjs";
 import { getParam } from "./utils.mjs";
 import productDetails from "./productDetails.mjs";
 
 const productId = getParam("product");
-// await productDetails(productId);
 init(productId);
 
-async function init(productId){
-  await productDetails(productId)
+async function init(productNo) {
+  await productDetails(productNo);
 }
 
-function addProductToCart(product) {
-  const cartData = getLocalStorage("so-cart") || [];
-  cartData.push(product);
-  setLocalStorage("so-cart", cartData);
-}
+// function addProductToCart(product) {
+//   const cartData = getLocalStorage("so-cart") || [];
+//   cartData.push(product);
+//   setLocalStorage("so-cart", cartData);
+// }
 
 // add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+// async function addToCartHandler(e) {
+//   const product = await findProductById(e.target.dataset.id);
+//   addProductToCart(product);
+// }
