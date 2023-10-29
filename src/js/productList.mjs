5e4a19edcdc6b39ecf47f9bbfb90dd3dfc1a1c2d
cplmills <91394219+cplmills@users.noreply.github.com>
2023-10-29 16:09:06 +1000

    added function to create breadcrumbs

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -24,3 +24,35 @@ export default async function productList(selector, category) {
     renderListWithTemplate(productCardTemplate, selector, Allproducts, "afterbegin", false);
     sortProduct(productCardTemplate, selector, Allproducts, "afterbegin", false);
 }
+
+export function createBreadcrumbs(breadcrumbsArray) {
+  // Get a reference to the breadcrumb container element
+  const breadcrumbContainer = document.getElementById('breadcrumbs');
+
+  // Clear the container
+  breadcrumbContainer.innerHTML = '';
+  breadcrumbContainer.classList.add("divider");
+  // Iterate through the breadcrumb array and create breadcrumb elements
+  breadcrumbsArray.forEach((breadcrumb, index) => {
+    const [name, link] = breadcrumb;
+
+    // Create a list item element
+    const listItem = document.createElement('li');
+
+    // Create a link element and capatalize the first letter of the name
+    // if the link is the current page (denoted by a `#` for the link element) set the style to activeBreadcrumb
+    const anchor = document.createElement('a');
+    anchor.textContent = name.charAt(0).toUpperCase() + name.slice(1);
+    anchor.href = link;
+    if (link === "#") {
+      anchor.id = "activeBreadcrumb";
+    }
+
+    // Append the link to the list item
+    listItem.appendChild(anchor);
+
+    // Append the list item to the breadcrumb container
+    breadcrumbContainer.appendChild(listItem);
+  });
+//  sortProduct(productCardTemplate, selector, Allproducts, "afterbegin", false);
+}
