6243336dc1acc13fd290b6585930f68aa0d2572b
cplmills <91394219+cplmills@users.noreply.github.com>
2023-10-29 16:36:40 +1000

    finished breadcrumbs, linted, and built

--- src/js/product-list.mjs
+++ src/js/product-list.mjs
@@ -1,8 +1,9 @@
 import productList from "./productList.mjs";
 import { loadHeaderFooter, getParam } from "./utils.mjs";
 import { createBreadcrumbs } from "./productList.mjs";
+
 productList(".product-list", getParam("product"));
-createBreadcrumbs([["Home", "../index.html"],[getParam("product"), `../product-list/index.html?product=${getParam("category")}`]]);
+createBreadcrumbs([["Home", "../index.html"],[getParam("product"), `#`]]);
 
 loadHeaderFooter();
 
--- src/js/product.js
+++ src/js/product.js
@@ -5,7 +5,7 @@ import { findCategoryById } from "./externalServices.mjs";
 
 const productId = getParam("product");
 productDetails(productId);
-let thisCategory = await findCategoryById(getParam("product", "Category"));
+let thisCategory = findCategoryById(getParam("product", "Category"));
 createBreadcrumbs([["Home", "../index.html"],[thisCategory, `../product-list/index.html?product=${thisCategory}`],[productId, "#"] ]);
 
 // function addProductToCart(product) {
